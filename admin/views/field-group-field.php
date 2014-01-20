f595c6b72bd8be0708b0de06b9fac0b6472c597d
Elliot Condon <e@elliotcondon.com>
2014-01-20 18:46:10 +1100

    Minor class name change
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- admin/views/field-group-field.php
+++ admin/views/field-group-field.php
@@ -36,7 +36,7 @@ $atts = array(
 			</strong>
 			<div class="row-options">
 				<span><a class="edit-field" title="<?php _e("Edit field",'acf'); ?>" href="#"><?php _e("Edit",'acf'); ?></a> |</span>
-				<span><a class="clone-field" title="<?php _e("Duplicate field",'acf'); ?>" href="#"><?php _e("Duplicate",'acf'); ?></a> | </span>
+				<span><a class="duplicate-field" title="<?php _e("Duplicate field",'acf'); ?>" href="#"><?php _e("Duplicate",'acf'); ?></a> | </span>
 				<span><a class="move-field" title="<?php _e("Move field to another group",'acf'); ?>" href="#"><?php _e("Move",'acf'); ?></a> | </span>
 				<span><a class="delete-field" title="<?php _e("Delete field",'acf'); ?>" href="#"><?php _e("Delete",'acf'); ?></a></span>
 			</div>
